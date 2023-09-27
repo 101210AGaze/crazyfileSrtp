2580d66d7ee3dcd498cd099206e1a024cfc6d6bc
xinlin <213212758@seu.edu.cn>
2023-09-27 14:40:52 +0800

    完整的boid提交

--- CTest/message_switch_type/message_switch_type.c
+++ CTest/message_switch_type/message_switch_type.c
@@ -2,13 +2,19 @@
 #include <string.h>
 
 int main() {
-    float originalFloat = 3.14159; // 你想要转换的float值
-    char floatChar[sizeof(float)]; // 创建一个与float大小相同的char数组
+    float originalFloat = 3.14159; // 你想要转换的 float 值
+    char floatChar[sizeof(float)]; // 创建一个与 float 大小相同的 char 数组
 
-    // 将float转换为char数组
+    // 将 float 转换为 char 数组
     memcpy(floatChar, &originalFloat, sizeof(float));
 
-    // 将char数组还原为float
+    printf("Float Char Sequence (Hexadecimal):\n");
+    for (size_t i = 0; i < sizeof(float); i++) {
+        printf("%02X ", (unsigned char)floatChar[i]);
+    }
+    printf("\n");
+
+    // 将 char 数组还原为 float
     float restoredFloat;
     memcpy(&restoredFloat, floatChar, sizeof(float));
 
